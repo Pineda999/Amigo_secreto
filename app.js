0475b3ffdaac1c92ff6e980c8ada2a6931f3e344
pineda999 <pinedamoises08@gmail.com>
2025-02-10 18:47:22 -0600

    actualizado

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let ListaAmigos=[];
+
+function agregarAmigo(){
+   let amigo =document.getElementById("amigo").value;
+
+   if(amigo==false){
+        alert("Por favor, digite un nombre valido");
+   }else{
+        ListaAmigos.push(amigo);
+        if (true){
+            Texto();
+            limpiarCampo();
+    }   
+        }
+    console.log(ListaAmigos);
+}
+
+function sortearAmigo(){
+    console.log("funciona sortear")
+}
+
+function limpiarCampo(){
+    document.getElementById("amigo").value="";
+}
+
+function Texto(){
+   let mostrarLista= document.querySelector("#amigo").value;
+   document.querySelector("#listaAmigos").innerHTML +="<li>"+mostrarLista+"</li>";
+    return;
+}
